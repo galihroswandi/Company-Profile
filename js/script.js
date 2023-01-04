1559726e065e279120db03478d0e040626f0f4ee
Galih Roswandi12 <galihroswandi12@gmail.com>
2023-01-04 10:45:07 +0700

    update reference and add javascript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Horizontal scroll
+const container = document.querySelector('#container-carousel');
+let scrollLine = document.querySelector('#scrollLine');
+
+container.addEventListener('wheel', e => {
+    e.preventDefault();
+    const value = container.scrollLeft += e.deltaY;
+    value <= 4511 ? container.scrollLeft += e.deltaY : null;
+})
