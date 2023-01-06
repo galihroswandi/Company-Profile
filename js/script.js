482a2f82c0c9599751cd59b5ed59184be093f6d8
Galih Roswandi <galihroswandi25@gmail.com>
2023-01-06 16:49:51 +0700

    tambah prev dan next

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Horizontal scroll
+// // Horizontal scroll
 // const container = document.querySelector('#container-carousel');
 // let scrollLine = document.querySelector('#scrollLine');
 
@@ -7,3 +7,26 @@
 //     const value = container.scrollLeft += e.deltaY;
 //     value <= 4511 ? container.scrollLeft += e.deltaY : null;
 // })
+
+
+// Navbar Toggle
+const toggleButton = document.querySelector('nav>footer>.toggle');
+const navList = document.querySelector('nav>footer>ul');
+
+toggleButton.addEventListener('click', () => {
+    if (navList.classList.contains('active')) {
+        navList.classList.remove('active')
+        navList.style.marginTop = '-5rem';
+        navList.style.transition = 'all .25s cubic-bezier(0.71, 0, 0.44, 1.46)';
+        navList.style.opacity = 0;
+        navList.style.transform = 'scale(.5)';
+    } else {
+        navList.classList.add('active')
+        navList.style.marginTop = '0';
+        navList.style.transition = 'all .25s cubic-bezier(0.71, 0, 0.44, 1.46)';
+        navList.style.opacity = 1;
+        navList.style.transform = 'scale(1)';
+    }
+
+})
+
