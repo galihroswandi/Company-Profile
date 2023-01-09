a0d1b43e76ff450e33f0bf83b0efc356287d73d5
Galih Roswandi <galihroswandi25@gmail.com>
2023-01-09 20:40:42 +0700

    update responsive slider

--- js/script.js
+++ js/script.js
@@ -33,6 +33,7 @@ toggleButton.addEventListener('click', () => {
 
 // Carousel
 const carousel = Array.from(document.querySelectorAll('#container-carousel>figure'));
+const carousel_wrapper = document.getElementById('container-carousel');
 
 let transformValue = 0;
 let trailValue = 0;
@@ -41,39 +42,80 @@ let interval = 4000;
 const slide = (condition) => {
 
     // hapus interval ketika tombol di klik
-    // clearInterval(start);
+    clearInterval(start);
 
     condition === "next" ? nextSlide() : prevSlide();
 
     move(transformValue, trailValue);
 
+    start = setInterval(() => slide('next'), interval);
+
 }
 
-// let start = setInterval(() => slide('next'), interval);
+let start = setInterval(() => slide('next'), interval);
 
 const nextSlide = () => {
-
-    transformValue === 50 ? transformValue = 0 : transformValue += 425;
+    nextXTransform();
 }
 
 const prevSlide = () => {
-    console.log('prev slide active');
+    prevXTransform();
+}
+
+const nextXTransform = () => {
+    if (Math.floor(window.innerWidth) >= 1440) {
+        transformValue === 850 ? transformValue = 0 : transformValue += 425;
+    } else if ((Math.floor(window.innerWidth) < 1440) && (Math.floor(window.innerWidth) >= 1095)) {
+        transformValue === 630 ? transformValue = 0 : transformValue += 315;
+    } else if ((Math.floor(window.innerWidth) < 1095) && (Math.floor(window.innerWidth) >= 830)) {
+        transformValue === 856 ? transformValue = 0 : transformValue += 214;
+    } else if ((Math.floor(window.innerWidth) < 830) && (Math.floor(window.innerWidth) >= 364)) {
+        transformValue === 856 ? transformValue = 0 : transformValue += 100;
+    }
+}
+
+const prevXTransform = () => {
+    if (Math.floor(window.innerWidth) >= 1440) {
+        transformValue === 0 ? transformValue = 850 : transformValue -= 425;
+    } else if ((Math.floor(window.innerWidth) < 1440) && (Math.floor(window.innerWidth) >= 1095)) {
+        transformValue === 0 ? transformValue = 630 : transformValue -= 315;
+    } else if ((Math.floor(window.innerWidth) < 1095) && (Math.floor(window.innerWidth) >= 830)) {
+        transformValue === 0 ? transformValue = 856 : transformValue -= 214;
+    } else if ((Math.floor(window.innerWidth) < 830) && (Math.floor(window.innerWidth) >= 364)) {
+        transformValue === 0 ? transformValue = 856 : transformValue -= 100;
+    }
 }
 
 const move = (tValue, trail) => {
-    // console.log(Math.floor(window.innerWidth));
-    if (Math.floor(window.innerWidth) >= 1440) {
-        
-        for(let i = 0; i <= carousel.length; i++){
-            i >= 0 && i < 4 ? console.log(carousel[i]) : null;
-            carousel[i].style.transform = `translateX(-${tValue}%)`;
-        }
-
-        // newSlide.style.transform = `translateX(-${tValue}%)`;
-        // console.log(newSlide);
-    }
+    carousel.forEach(el => {
+        el.style.transform = `translateX(-${tValue}%)`;
+    })
 }
 
 document.querySelectorAll('svg').forEach(nop => {
     nop.addEventListener('click', () => nop.classList.contains('next') ? slide('next') : slide('prev'));
 })
+
+
+const touchSlide = (() => {
+    let start, move, change, sliderWidth;
+
+    carousel_wrapper.addEventListener('touchstart', e => {
+        start = e.touches[0].clientX;
+        sliderWidth = carousel_wrapper.clientWidth / 4;
+    })
+
+    carousel_wrapper.addEventListener('touchmove', e => {
+        e.preventDefault();
+        move = e.touches[0].clientX;
+        change = start - move;
+    })
+
+    const mobile = (e) => {
+        change > (sliderWidth / 4) ? slide('next') : null;
+        (change * -1) > (sliderWidth / 4) ? slide('prev') : null;
+        [start, move, change, sliderWidth] = [0, 0, 0, 0];
+    }
+
+    carousel_wrapper.addEventListener('touchend', mobile);
+})();
